8aba3620c1c830cfdde408eab583f009cc366fad
João Alves <jneto2727@gmail.com>
2021-04-23 11:13:50 -0300

    initial commit

---
+++ packages/mobile/metro.config.js
@@ -0,0 +1,48 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+
+ const blacklist = require('metro-config/src/defaults/blacklist');
+ const path = require('path');
+
+
+const blacklistRE = blacklist([
+  /node_modules\/.*\/node_modules\/react-native\/.*/,
+]);
+
+ module.exports = (async () => {
+  return {
+    projectRoot: path.resolve(__dirname, '.'),
+    watchFolders: [
+      path.resolve(__dirname, '../../node_modules'),
+      // path.resolve(__dirname, '../core'), // Add your packages Here!
+    ],
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      blacklistRE,
+    },
+  };
+
+ })()
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
